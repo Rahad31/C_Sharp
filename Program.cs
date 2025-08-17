28fe0505f0790fc1cd0772b4fa934662f2c76650
Md Rahad <rahad3100@gmail.com>
2025-08-18 04:19:07 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -49,6 +49,35 @@
             }
 
 
+            // For smallest number
+
+            int smallestIndex;
+
+            if (numbers[0] <= numbers[1] && numbers[0] <= numbers[2])
+            { smallestIndex = 0; }
+
+            else if (numbers[1] <= numbers[0] && numbers[1] <= numbers[2])
+            { smallestIndex = 1; }
+
+            else
+            { smallestIndex = 2; }
+
+
+            int smallest=0;
+            switch (smallestIndex)
+            {
+                case 0:
+                    smallest = numbers[0];
+                    break;
+                case 1:
+                    smallest = numbers[1];
+                    break;
+                case 2:
+                    smallest = numbers[2];
+                    break;
+            }
+
+            Console.WriteLine("The smallest number is: "+smallest);
         }
     }
 }
